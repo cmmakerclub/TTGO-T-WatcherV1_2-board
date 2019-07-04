da5dd18511537763bf102aaf456411366331bda6
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-07-04 15:56:54 +0700

    renew ui TTGO T-watch v 1.2

---
+++ build/24-0a-c4-a6-f6-50/glcdfont.d
@@ -0,0 +1,2 @@
+C:\Users\Asus\Documents\KBProIDE\boards\ttgo-t-watcher-v1.2\build\24-0a-c4-a6-f6-50\glcdfont.o: \
+ C:\Users\Asus\Documents\KBProIDE\boards\ttgo-t-watcher-v1.2\include\glcdfont.c
---
+++ build/ff-ff-ff-ff-ff-ff/glcdfont.d
@@ -0,0 +1,2 @@
+C:\Users\Asus\Documents\KBProIDE\boards\ttgo-t-watcher-v1.2\build\ff-ff-ff-ff-ff-ff\glcdfont.o: \
+ C:\Users\Asus\Documents\KBProIDE\boards\ttgo-t-watcher-v1.2\include\glcdfont.c
