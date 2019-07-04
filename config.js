3f348976d26f861df671862b489919911f44ad42
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-07-04 23:47:56 +0700

    update picture

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
   website: "http://www.kbpro.org",
   email: "makerasia@gmail.com",
   git: "https://github.com/cmmakerclub/ttgo-t-watcher-v1.2-board/",
-  image: "/static/display.jpg",
+  image: "/static/display.JPG",
   version: "1.0.2",
 };
